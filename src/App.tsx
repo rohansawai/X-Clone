8764adebf66599bf2fa2a478f73918c30b721753
Rohan Sawai <sawairohan90@gmail.com>
2023-10-09 22:45:20 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Footer from "./layouts/footer";
+import Header from "./layouts/header";
+
+function App() {
+  return (
+    <div>
+      <h1>Twitter Clone</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/layouts/header.tsx
@@ -0,0 +1,73 @@
+import {
+  Navbar,
+  Typography,
+} from "@material-tailwind/react";
+
+function NavList() {
+  return (
+    <ul className="my-2 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a
+          href="#"
+          className="flex items-center hover:text-blue-500 transition-colors"
+        >
+          Dark Theme
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a
+          href="#"
+          className="flex items-center hover:text-blue-500 transition-colors"
+        >
+          Account
+        </a>
+      </Typography>
+      <Typography
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-medium"
+      >
+        <a
+          href="#"
+          className="flex items-center hover:text-blue-500 transition-colors"
+        >
+          Download
+        </a>
+      </Typography>
+    </ul>
+  );
+}
+
+const Header = () => {
+
+  return (
+    <Navbar className="h-10 px-6 py-1 bg-blue-gray-100  ">
+      <div className="flex items-center justify-between text-blue-gray-900">
+        <Typography
+          as="a"
+          href="#"
+          variant="h6"
+          className="mr-4 py-1.5"
+        >
+          US Plasma Center data
+        </Typography>
+        <div className="hidden lg:block">
+          <NavList />
+        </div>
+      </div>
+    </Navbar>
+  );
+}
+
+export default Header;
