5ca05e4157f87caca17f31124d2d9fae211810d2
Rohan Sawai <sawairohan90@gmail.com>
2023-10-16 23:53:04 +0530

    changes

--- src/layouts/rhs.tsx
+++ src/layouts/rhs.tsx
@@ -3,7 +3,7 @@ import Search from "../services/utils/search";
 const Rhs = () => {
 
     return (
-        <div>
+        <div style={{backgroundColor: "beige"}}>
             <h1>RHS</h1>
             <Search></Search>
         </div>
